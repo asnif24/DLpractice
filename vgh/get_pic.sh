08803c1acf4760d746252c0c02607aba191638b4
Asnif Lin <asnif24@gmail.com>
2017-11-25 15:00:30 +0800

    del vgh2

--- vgh/get_pic.sh
+++ vgh/get_pic.sh
@@ -1,2 +1,1 @@
-echo ohya`date +%s` >> /home/asnif/DLpractice/vgh2/check.txt
-wget https://www6.vghtpe.gov.tw/opd/servlet/opd.vImage -O /home/asnif/DLpractice/vgh2/pic/`date +%s`.vImage 
+wget https://www6.vghtpe.gov.tw/opd/servlet/opd.vImage -O /home/asnif/DLpractice/vgh/pic/`date +%s`.vImage 
